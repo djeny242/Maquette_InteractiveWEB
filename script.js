0c00ac31d468f70f9eb9fb142460e6276ee7ee3e
Djeny <Djeny292@gmail.com>
2025-04-09 12:11:17 +0200

    cv modification

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 const bouton = document.getElementById("btn3");
 function downloadPDF() {
-  const element = document.getElementById("container");
-  html2pdf()
-    .from(element)
-    .save("Mon CV.pdf") ;  // .then(() => {
+    const element = document.getElementById("container");
+    html2pdf()
+        .from(element)
+        .save("Mon CV.pdf");  // .then(() => {
     //   if (localStorage.getItem("count")) {
     //     count = +localStorage.getItem("count");
     //   }
@@ -14,5 +14,12 @@ function downloadPDF() {
     //   counter.innerText = localStorage.getItem("count");
     // });
 }
+const bntChangerCouleur = document.getElementById("btn2");
+bouton.addEventListener("click", downloadPDF);
 
-bouton.addEventListener("click" , downloadPDF);
+const changerCouleur = (couleur) => {
+    bntChangerCouleur.addEventListener("click", () => {
+        document.documentElement.style.setProperty('--bg-color', couleur);
+    })
+
+}
